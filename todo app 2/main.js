05712506a06974c497cdb628c92b7fda760db8c9
christineJadeAvila <christinejadeavila05@gmail.com>
2023-06-02 21:13:21 +0800

    Movies Storage

--- todo app 2/main.js
+++ todo app 2/main.js
@@ -7,93 +7,94 @@ let tasks = document.getElementById("tasks");
 let add = document.getElementById("add");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    formValidation();
+  e.preventDefault();
+  formValidation();
+});
+
+let formValidation = () => {
+  if (textInput.value === "") {
+    console.log("failure");
+    msg.innerHTML = "Task cannot be blank";
+  } else {
+    console.log("success");
+    msg.innerHTML = "";
+    acceptData();
+
+    add.setAttribute("data-bs-dismiss", "modal");
+    add.click();
+
+    (() => {
+    add.setAttribute("data-bs-dismiss", "");
+    })();
+
+  }
+};
+
+let data = [];
+
+let acceptData = () => {
+  data.push({
+    text: textInput.value,
+    date: dateInput.value,
+    description: textarea.value
   });
-  
-  let formValidation = () => {
-    if (textInput.value === "") {
-      console.log("failure");
-      msg.innerHTML = "Task cannot be blank";
-    } else {
-      console.log("success");
-      msg.innerHTML = "";
-      acceptData();
 
-      add.setAttribute("data-bs-dismiss", "modal");
-      add.click();
-  
-      (() => {
-        add.setAttribute("data-bs-dismiss", "");
-      })();
+  localStorage.setItem(
+    "data",
+    JSON.stringify(data)
+  );
 
-    }
-  };
-
-  let data = [];
-
-  let acceptData = () => {
-    data.push({
-        text: textInput.value,
-        date: dateInput.value,
-        description: textarea.value
-    });
-
-    localStorage.setItem(
-        "data",
-        JSON.stringify(data)
-    );
-
-    console.log(data);
-    createTasks();
+  console.log(data);
+  createTasks();
 }
 
 let createTasks = () => {
-  tasks.innerHTML = "";
-  data.map((x, y) => {
-    return (tasks.innerHTML += `
+tasks.innerHTML = "";
+data.map((x, y) => {
+  return (tasks.innerHTML += `
     <div id=${y}>
-          <span class="fw-bold">${x.text}</span>
-          <span class="small text-secondary">${x.date}</span>
-          <p>${x.description}</p>
-  
-          <span class="options">
-            <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
-            <i onClick ="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
-          </span>
-        </div>
-    `);
-  });
+    <span class="fw-bold">${x.text}</span>
+    <span class="small text-secondary">${x.date}</span>
+    <p>${x.description}</p>
+
+    <span class="options">
+    <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+    <i onClick ="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
+    </span>
+    </div>
+  `);
+});
 
   resetForm();
 };
 
 let resetForm = () => {
-    textInput.value = "";
-    dateInput.value = "";
-    textarea.value = "";
+  textInput.value = "";
+  dateInput.value = "";
+  textarea.value = "";
 }
 
 let deleteTask = (e) => {
-    e.parentElement.parentElement.remove();
+  e.parentElement.parentElement.remove();
 
-    data.splice(e.parentElement.parentElement.id, 1);
-    localStorage.setItem("data", JSON.stringify(data));
+  data.splice(e.parentElement.parentElement.id, 1);
+  localStorage.setItem("data", JSON.stringify(data));
 
-    console.log(data);
+  console.log(data);
 }
+
 let editTask = (e) => {
-    let selectedTask = e.parentElement.parentElement;
-  
-    textInput.value = selectedTask.children[0].innerHTML;
-    dateInput.value = selectedTask.children[1].innerHTML;
-    textarea.value = selectedTask.children[2].innerHTML;
-  
-    deleteTask(e);
-  };
+  let selectedTask = e.parentElement.parentElement;
+
+  textInput.value = selectedTask.children[0].innerHTML;
+  dateInput.value = selectedTask.children[1].innerHTML;
+  textarea.value = selectedTask.children[2].innerHTML;
+
+  deleteTask(e);
+};
 
 (() => {
-    data = JSON.parse(localStorage.getItem("data")) || [];
-    console.log(data);
-    createTasks();
-  })();
+  data = JSON.parse(localStorage.getItem("data")) || [];
+  console.log(data);
+  createTasks();
+})();
